bcde1e41ca60e35f85a81dfc4039e3121cc69754
Thiago Lemos <thiagolemos1995@hotmail.com>
2024-01-18 22:36:42 -0300

    feat: added new controllers and services for create and to list clients  by name

--- client-service/src/app/clients/services/clients.service.ts
+++ client-service/src/app/clients/services/clients.service.ts
@@ -1,12 +1,52 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Pool } from 'pg';
+import { CreateClient } from '../models';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class ClientsService {
   constructor(@Inject('PG_CONNECTION') private conn: Pool) {}
 
   async getClients() {
-    const response = await this.conn.query('SELECT * FROM clients');
-    return response.rows;
+    try {
+      const response = await this.conn.query('SELECT * FROM clients');
+      return response.rows;
+    } catch (error) {
+      const formattedError = error as AxiosError;
+      return {
+        status: 500,
+        message: formattedError.stack,
+      };
+    }
+  }
+
+  async getClientById(name: string) {
+    try {
+      const response = await this.conn.query(
+        `SELECT * FROM clients WHERE name LIKE '%${name}%'`,
+      );
+      return response.rows;
+    } catch (error) {
+      const formattedError = error as AxiosError;
+      return {
+        status: 500,
+        message: formattedError.stack,
+      };
+    }
+  }
+
+  async createClient(createClient: CreateClient) {
+    try {
+      const response = await this.conn.query(
+        `INSERT INTO clients (name, email, phone) VALUES ('${createClient.name}', '${createClient.email}', '${createClient.phone}') RETURNING *`,
+      );
+      return response.rows;
+    } catch (error) {
+      const formattedError = error as AxiosError;
+      return {
+        status: 500,
+        message: formattedError.stack,
+      };
+    }
   }
 }
